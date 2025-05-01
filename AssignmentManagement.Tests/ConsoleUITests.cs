d577bbba76dbc1129c3a8305e824fc156dbebdac
Kathy Yost <kathyiyost@gmail.com>
2025-05-01 12:48:06 -0700

    Add .gitignore and unit tests file

---
+++ AssignmentManagement.Tests/ConsoleUITests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using Moq;
+using AssignmentManagement.Core;
+using AssignmentManagement.UI;
+
+namespace AssignmentManagement.Tests
+{
+    public class ConsoleUITests
+    {
+        [Fact]
+        public void DeleteAssignment_ShouldReturnTrue()
+        {
+            // Arrange
+            var mockService = new Mock<IAssignmentService>();
+
+            mockService.Setup(s => s.DeleteAssignment("Delete Me")).Returns(true);
+
+            var consoleUI = new ConsoleUI(mockService.Object);
+
+            // Act
+            var result = mockService.Object.DeleteAssignment("Delete Me");
+
+            // Assert
+            Assert.True(result);
+            mockService.Verify(s => s.DeleteAssignment("Delete Me"), Times.Once);
+        }
+    }
+}
