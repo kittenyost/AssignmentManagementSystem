16ef1dc77bdb61b17c07fc3448cab0c6914e8769
Kathy Yost <kathyiyost@gmail.com>
2025-05-01 09:19:53 -0700

    add interface

---
+++ AssignmentManagement.Core/IAssignmentService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentManagement.Core
+{
+    public interface IAssignmentService
+    {
+        public bool AddAssignment(Assignment assignment);
+
+
+        public List<Assignment> ListAll();
+
+
+        public List<Assignment> ListIncomplete();
+       
+
+        // TODO: Implement method to find an assignment by title
+        public Assignment FindAssignmentByTitle(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement method to mark an assignment complete
+        public bool MarkAssignmentComplete(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement method to delete an assignment by title
+        public bool DeleteAssignment(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement method to update an assignment (title and description)
+        public bool UpdateAssignment(string oldTitle, string newTitle, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
